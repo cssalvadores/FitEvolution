a6d607ad4646fcc13cdbbb13ee70cb8f58001904
Ageu Paim <ageupaim@gmail.com>
2024-10-03 11:23:44 +0100

    InitialDb

---
+++ FitEvolution/Data/Entities/DataContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore; 
+
+namespace FitEvolution.Data.Entities
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        // Adicione outras entidades aqui, como Tópicos, Respostas, Aulas, etc.
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.Username)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Password)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Sua_Connection_String");
+        }
+
+    }
+}
---
+++ FitEvolution/Data/Entities/UserRole.cs
@@ -0,0 +1,28 @@
+﻿using Google.Apis.Admin.Directory.directory_v1.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitEvolution.Data.Entities
+{
+    public enum UserRole
+    {
+        Admin,
+        Instructor,
+        User
+
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        //[Display(Username = "email")]
+        //[Display(Password = "Palavra-passe")]
+        //[MaxLength(50, ErrorMessage = "O campo {0} deve ter o máximo {1} caractéres.")]
+        //[Required(ErrorMessage = " O campo {0} é obrigatório.")]
+        public string? Username { get; set; }
+        public  string? Password { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+
+}
